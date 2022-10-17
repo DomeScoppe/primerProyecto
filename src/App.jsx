6c2a62e03e001b425e98ab1da55287c680435e43
osso <fdantealvarezt@gmail.com>
2022-10-17 18:23:45 -0500

    product card

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import './index.css'
+import Products from "./components/Products";
+
 function App() {
-
-
-  
   return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-
-  )
+    <>
+      <Products />
+    </>
+  );
 }
 
-export default App
+export default App;
